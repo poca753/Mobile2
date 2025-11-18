6f88f8c6cd9bfba1454932b325310ff8f959761e
poca753 <165678402+poca753@users.noreply.github.com>
2025-11-18 09:50:43 -0300

    tyrty
    yrt5y6er54

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
 function app() {
-    const [message, setMessage]
+    const [message, setMessage] = useState("Olá, mundo")
+    return (
+        <div>
+            <h1>{message}</h1>
+            <button onClick={
+                ()=>{setMessage("olá fui clicado");}}
+                ></button>
+        </div>
+    )
 }
+export default App
